a048a7b24bbdea6ce6e3f96c997afb9cc00435ff
Noah Greaves <noah@immersivetech.co>
2026-07-22 22:16:19 -0700

    added player scripts

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,51 @@
+using System.Collections.Specialized;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _moveSpeed = 1.6f;
+    [SerializeField] private float _accesleration = 12f;
+    [SerializeField] private float _isoAngle;
+
+    private Rigidbody _rb;
+    private Collider _collider;
+
+    private Player_Controls _playerControls;
+
+    private Vector2 _inputVector;
+    private Vector3 _targetVelocity;
+
+    private void Awake()
     {
-        
+        _playerControls = new Player_Controls();
+    }
+    
+    private void OnEnable()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+        _playerControls.Player.Movement.performed += OnMove;
+        _playerControls.Player.Movement.canceled += OnMove;
+
+        _playerControls.Player.Movement.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
+    {
+        _playerControls.Player.Movement.performed -= OnMove;
+        _playerControls.Player.Movement.canceled -= OnMove;
+
+        _playerControls.Player.Movement.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        print($"Movement Input: {context.ReadValue<Vector2>()}");
+        _inputVector = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
     {
         
     }
