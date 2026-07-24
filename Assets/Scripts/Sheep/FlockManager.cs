008d711147db3828ef6648403e25ff8af5424f8f
Allie Shackelford <allieshackelford@gmail.com>
2026-07-23 22:32:49 -0700

    add collider/rigidbody to sheep and extend movement to include separation forces

--- Assets/Scripts/Sheep/FlockManager.cs
+++ Assets/Scripts/Sheep/FlockManager.cs
@@ -58,4 +58,9 @@ public class FlockManager : MonoBehaviour
         return _lures;
     }
 
+    public List<Sheep> GetCurrentFlock()
+    {
+        return _flock;
+    }
+
 }
--- Assets/Scripts/Sheep/Sheep.cs
+++ Assets/Scripts/Sheep/Sheep.cs
@@ -11,18 +11,32 @@ public enum SheepState
 
 public class Sheep : MonoBehaviour
 {
-    private SheepState _state;
-    private FlockManager _flockManager;
+    
     [SerializeField] private float _moveSpeed = 3f;
+    [SerializeField] private float _acceleration = 2.0f;
     [SerializeField] private float _turnSpeed = 2f;
+    [SerializeField] private float _reachedDestRadius = 1.5f;
 
     [Tooltip("The amount of influence needed for a sheep to be attracted by a lure")]
     [SerializeField] private float _lureThreshold = 2f;
-    private Vector3 _currentTarget;
-
     // Wander 
     [SerializeField] private float _wanderRadius = 2;
+    [SerializeField] private float _wanderForce = 0.3f;
     private bool _waitingForNewTarget = true;
+    
+    // Separation
+    [SerializeField] private float _separationRadius = 1.5f;
+    [SerializeField] private float _separationForce = 2.0f;
+
+    // Ground Calcs
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundRayDistance = 5f;
+    [SerializeField] private float _groundOffset = 0.05f;
+
+    private Vector3 _currentTarget;
+    private Vector3 _velocity;
+    private SheepState _state;
+    private FlockManager _flockManager;
 
     // Animations
     private Animator _sheepAnimator;
@@ -39,7 +53,7 @@ public class Sheep : MonoBehaviour
     void Start()
     {
         _sheepAnimator = GetComponent<Animator>();
-        _state = SheepState.Idle;
+        SetState(SheepState.Wander);
     }
 
     public void SetState(SheepState newState)
@@ -48,45 +62,67 @@ public class Sheep : MonoBehaviour
 
         if (_state == SheepState.Wander)
         {
-            GetWanderTarget();
+            Invoke(nameof(GetWanderTarget), Random.Range(1,5));
         }
     }
 
-    void Idle()
+    private Vector3 Separation()
+    {
+        Vector3 separation = Vector3.zero;
+        foreach (Sheep other in _flockManager.GetCurrentFlock())
+        {
+            if (other == this)
+                continue;
+
+            Vector3 distanceFromSheep = transform.position - other.transform.position;
+            float distance = distanceFromSheep.magnitude;
+            if (distance < _separationRadius)
+            {
+                separation += distanceFromSheep.normalized / distance;
+            }
+        }
+
+        return separation;
+    }
+
+    private void Idle()
     {
         
     }
 
-    void Wander()
+    private Vector3 Wander()
     {
         if (_waitingForNewTarget)
         {
-            return;
+            return Vector3.zero;
         }
 
         Vector3 distanceFromTarget = _currentTarget - transform.position;
-        if (distanceFromTarget.magnitude <= 0.1f)
+        if (distanceFromTarget.magnitude <= 0.5f)
         {
             // Reached wander target, pick another
             _waitingForNewTarget = true;
-            _sheepAnimator.SetBool("IsMoving", false);
-            Invoke(nameof(GetWanderTarget), Random.Range(1,5));
-            return;
+            _velocity = Vector3.zero;
+            Invoke(nameof(GetWanderTarget), Random.Range(1,10));
+            return Vector3.zero;
         }
 
-        Vector3 direction = distanceFromTarget.normalized;
-        transform.position += direction * _moveSpeed * Time.deltaTime;
-        transform.forward = Vector3.Lerp(transform.forward, direction, Time.deltaTime * _turnSpeed);
+        return distanceFromTarget.normalized;
+    }
+    private void GetWanderTarget()
+    {
+        _waitingForNewTarget = false;
+        Vector3 currentHomingTarget = _flockManager.GetTargetPoint();
+        Vector2 randomOffset = Random.insideUnitCircle * _wanderRadius;
+        _currentTarget = currentHomingTarget + new Vector3(randomOffset.x, 0f, randomOffset.y);
     }
 
-    void CheckLureInfluence()
+    private void CheckLureInfluence()
     {
         foreach(Lure lure in _flockManager.GetCurrentLures())
         {
             Vector3 distanceFromTarget = lure.transform.position - transform.position;
             float influence = lure.LureStrength * (1 - distanceFromTarget.magnitude / lure.Radius);
-            Debug.Log($"Distance from lure: {distanceFromTarget.magnitude}");
-            Debug.Log($"Influence: {influence}");
             if (influence >= _lureThreshold)
             {
                 _currentTarget = lure.transform.position;
@@ -96,48 +132,81 @@ public class Sheep : MonoBehaviour
         } 
     }
 
-    void Lure()
+    private Vector3 Lure()
     {
         Vector3 distanceFromTarget = _currentTarget - transform.position;
-        if (distanceFromTarget.magnitude <= 0.1f)
+        if (distanceFromTarget.magnitude <= 0.5f)
         {
+            _velocity = Vector3.zero;
+            return Vector3.zero;
+        }
+
+        return distanceFromTarget.normalized;
+    }
+
+    private void Move(Vector3 sheepSteering)
+    {
+        sheepSteering.y = 0;
+
+        _velocity = Vector3.Lerp(_velocity, sheepSteering.normalized * _moveSpeed, Time.deltaTime * _acceleration);
+
+        if (_velocity.sqrMagnitude < 0.01f)
+        {
+            _velocity = Vector3.zero;
             _sheepAnimator.SetBool("IsMoving", false);
             return;
         }
 
-        Vector3 direction = distanceFromTarget.normalized;
-        transform.position += direction * _moveSpeed * Time.deltaTime;
-        transform.forward = Vector3.Lerp(transform.forward, direction, Time.deltaTime * _turnSpeed);
+        _sheepAnimator.SetBool("IsMoving", true);
+
+        transform.position += _velocity * Time.deltaTime;
+
+        if (_velocity.sqrMagnitude > 0.01f)
+        {
+            Vector3 direction = _velocity.normalized;
+            transform.forward = Vector3.Lerp(transform.forward, direction, Time.deltaTime * _turnSpeed);
+        }
+    }
+
+    private void UpdateGroundPosition()
+    {
+        Ray rayToGround = new Ray(transform.position + Vector3.up, Vector3.down);
+
+        if (Physics.Raycast(rayToGround, out RaycastHit hit, _groundRayDistance, _groundLayer))
+        {
+            Vector3 position = transform.position;
+            position.y = hit.point.y + _groundOffset;
+
+            transform.position = position;
+        }
     }
 
     void Update()
     {
+        Vector3 sheepSteer = Vector3.zero;
+
         switch(_state)
         {
             case SheepState.Idle: 
                 break;
             case SheepState.Wander: 
-                Wander();
+                sheepSteer += Wander() * _wanderForce;
                 break;
             case SheepState.Lure:
-                Lure();
+                sheepSteer += Lure();
                 break;
             default:
                 break;
         }
 
+        sheepSteer += Separation() * _separationForce;
+
+        Move(sheepSteer);
+        UpdateGroundPosition();
+
         if (_state != SheepState.Lure)
         {
             CheckLureInfluence();
         }
-    }
-
-    private void GetWanderTarget()
-    {
-        _waitingForNewTarget = false;
-        _sheepAnimator.SetBool("IsMoving", true);
-        Vector3 currentHomingTarget = _flockManager.GetTargetPoint();
-        Vector2 randomOffset = Random.insideUnitCircle * _wanderRadius;
-        _currentTarget = currentHomingTarget + new Vector3(randomOffset.x, 0f, randomOffset.y);
     }
 }
