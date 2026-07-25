593c329fb1b82d53e2370ed45c2dda656dbe6662
Noah Greaves <noah@immersivetech.co>
2026-07-24 20:40:00 -0700

    new fence prefab

--- Assets/Scripts/Fence.cs
+++ Assets/Scripts/Fence.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Fence : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int _hitPoints = 2;
+    [SerializeField] private GameObject _fenceModel;
+
+    private int _currentHitPoints;
+
+    private void Awake()
     {
-        
+        _currentHitPoints = _hitPoints;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Damage() 
     {
-        
+        _currentHitPoints -= 1;
+        if (_currentHitPoints <= 0)
+        {
+            // Destroy Fence
+            // Play Fence Destruction Animation
+
+            _fenceModel.SetActive(false);
+        }
+    }
+
+    public void RepairFence() 
+    {
+        _currentHitPoints = _hitPoints;
+        _fenceModel.SetActive(true);
     }
 }
