2a6402d6e49e0d832102a23da705cf0c21a4acfa
Allie Shackelford <allieshackelford@gmail.com>
2026-07-26 01:20:32 -0700

    end screen text animate

--- Assets/Scripts/UI/EndScreen.cs
+++ Assets/Scripts/UI/EndScreen.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -5,10 +6,15 @@ public class EndScreen : MonoBehaviour
 {
 
     [SerializeField] TextMeshProUGUI _endText;
+
+    private FlockManager _flockManager;
+
+    private float charactersPerSecond = 5f;
     void Start()
     {
         GameManager.Instance.OnGameStateChanged += SetEndScreenState;
         gameObject.SetActive(false);
+       _flockManager = FindAnyObjectByType<FlockManager>();
     }
 
     void SetEndScreenState(GameManager.GameState state)
@@ -16,11 +22,24 @@ public class EndScreen : MonoBehaviour
         gameObject.SetActive(state == GameManager.GameState.GameWon || state == GameManager.GameState.GameLost);
         if (state == GameManager.GameState.GameWon)
         {
-            _endText.text = "You Have Won";
+            _endText.text = $"You Survived\n {_flockManager.GetSheepCount()} Sheep Saved";
+            _endText.maxVisibleCharacters = 0;
+            StartCoroutine(TypeRoutine());
         }
         else if (state == GameManager.GameState.GameLost)
         {
-            _endText.text = "You Have Lost";
+            _endText.text = $"You Have Lost";
+            _endText.maxVisibleCharacters = 0;
+            StartCoroutine(TypeRoutine());
+        }
+    }
+
+    IEnumerator TypeRoutine()
+    {
+        while (_endText.maxVisibleCharacters < _endText.text.Length)
+        {
+            _endText.maxVisibleCharacters++;
+            yield return new WaitForSeconds(1f / charactersPerSecond);
         }
     }
 
