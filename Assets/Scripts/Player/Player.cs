2cf64fc6bdedd09d5e5e233bd3fd3bfea290a2e6
Allie Shackelford <allieshackelford@gmail.com>
2026-07-25 23:45:08 -0700

    disable player input until game start

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -53,7 +53,7 @@ public class Player : MonoBehaviour
         _playerControls.Player.BellLure.performed += OnBellLure;
         _playerControls.Player.BellLure.canceled += OnBellLure;
 
-        _playerControls.Player.Enable();
+        _playerControls.Player.Disable();
     }
 
     private void UnsubscribeInputs()
@@ -138,6 +138,19 @@ public class Player : MonoBehaviour
     private void CancelLureAnim()
     {
         SetLureAnim(false);
+    }
+
+    void Start()
+    {
+        GameManager.Instance.OnGameStateChanged += StateChange;
+    }
+
+    private void StateChange(GameManager.GameState state)
+    {
+        if (state == GameManager.GameState.LevelStart)
+        {
+            _playerControls.Player.Enable();
+        }
     }
 
     private void Update()
